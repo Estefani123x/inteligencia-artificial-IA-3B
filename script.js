720218b7bb7b09f31587ec0bfb1674b4053b0b94
Estefani123x <159539446+Estefani123x@users.noreply.github.com>
2025-08-29 08:04:17 -0300

    asd

---
+++ script.js
@@ -0,0 +1,5 @@
+// Código JavaScript adicional para interações ou animações personalizadas
+document.getElementById('btn_interacao').addEventListener('click', function () {
+  alert('Muito obrigado pela sua contribuição!');
+});
+
